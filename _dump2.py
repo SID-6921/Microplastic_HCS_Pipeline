ba2d1cf81f0869b8ac99e37f5ca80efd8bff3583
SID <nandasiddhardha@gmail.com>
2026-05-26 15:18:11 -0400

    chore: remove scratch _dump*.py inspection scripts (committed accidentally in r3)

--- _dump2.py
+++
@@ -1,9 +0,0 @@
-﻿from docx import Document
-d = Document('docs/EI_Manuscript_FINAL.docx')
-for i in [7, 51, 64, 92, 93, 116, 117]:
-    print(f"--- PARA {i} ---")
-    print(repr(d.paragraphs[i].text))
-    print()
-print("--- §4.5 context ---")
-for i in range(108, 115):
-    print(i, repr(d.paragraphs[i].text[:600]))
